2e80f219d5bb69761040084dfde1e4eea8e381df
Rafael <Rafaelblink@gmail.com>
2019-07-20 00:36:06 -0300

    adding swagger

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.PORT || 3000);
 
-  if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => app.close());
-  }
+  const options = new DocumentBuilder()
+    .setTitle('E-COMMERCE IONIC')
+    .setDescription('API para meus tesouros utilizarem no treinamento Indra')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('swagger', app, document);
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
