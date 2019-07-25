2d03f2bbafe33ca95bccbab8dc53d15dd2ce1d62
Rafael <Rafaelblink@gmail.com>
2019-07-25 20:33:54 -0300

    adding annotations for swagger properties of model

--- src/product/product.entity.ts
+++ src/product/product.entity.ts
@@ -1,27 +1,35 @@
 import { Column, Entity, ObjectID, ObjectIdColumn } from 'typeorm';
+import { ApiModelProperty } from '@nestjs/swagger';
 
 @Entity('products')
 export class Product {
-  
+
+  @ApiModelProperty()
   @ObjectIdColumn()
   id: ObjectID;
 
+  @ApiModelProperty()
   @Column()
   productName: string;
 
+  @ApiModelProperty()
   @Column()
   englishName: string;
-  
+
+  @ApiModelProperty()
   @Column()
   description: string;
 
+  @ApiModelProperty()
   @Column()
   price: number;
 
+  @ApiModelProperty()
   @Column()
   quantity: number;
 
+  @ApiModelProperty()
   @Column()
   unity: string;
-  
+
 }
--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,17 +1,22 @@
 import { Column, Entity, ObjectID, ObjectIdColumn } from 'typeorm';
+import { ApiModelProperty } from '@nestjs/swagger';
 
 @Entity('users')
 export class User {
-  
+
+  @ApiModelProperty()
   @ObjectIdColumn()
   id: ObjectID;
 
+  @ApiModelProperty()
   @Column()
   username: string;
 
+  @ApiModelProperty()
   @Column()
   password: string;
-
+  
+  @ApiModelProperty()
   @Column()
-  isActive: boolean
+  isActive: boolean;
 }
